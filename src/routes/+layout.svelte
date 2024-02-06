b1f3e3995c7fc0c6b058cf5c24398d25301afa18
FreeDistortion <clouderneering@gmail.com>
2024-02-06 14:18:14 +0900

    DOM loading error fixed
    onMount(anfn)으로 해결

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,51 +1,45 @@
 <script lang="ts">
 	import '../app.css';
 	import { onMount, onDestroy } from 'svelte';
-  import { browser } from '$app/environment';
-  import { goto } from '$app/navigation';
+	import { browser } from '$app/environment';
+	import { goto } from '$app/navigation';
 
-  let timeoutId: ReturnType<typeof setTimeout> | undefined;
+	let timeoutId: ReturnType<typeof setTimeout> | undefined;
 
-  const resetTimer = (): void => {
-    if (timeoutId !== undefined) {
-      clearTimeout(timeoutId);
-    }
-    timeoutId = setTimeout(() => {
-      goto('/');
-    }, 60*1000); // 60초 후에 /about으로 이동
-  };
+	const resetTimer = (): void => {
+		if (timeoutId !== undefined) {
+			clearTimeout(timeoutId);
+		}
+		timeoutId = setTimeout(() => {
+			goto('/');
+		}, 60 * 1000); // 60초 후에 /about으로 이동
+	};
 
-  if (browser) {
-    onMount(() => {
-      resetTimer();
-      window.addEventListener('click', resetTimer);
-      window.addEventListener('keypress', resetTimer);
-      window.addEventListener('mousemove', resetTimer);
-      window.addEventListener('scroll', resetTimer);
+	if (browser) {
+		onMount(() => {
+			resetTimer();
+			window.addEventListener('click', resetTimer);
+			window.addEventListener('keypress', resetTimer);
+			window.addEventListener('mousemove', resetTimer);
+			window.addEventListener('scroll', resetTimer);
+		});
+
+		onDestroy(() => {
+			if (timeoutId !== undefined) {
+				clearTimeout(timeoutId);
+			}
+			window.removeEventListener('click', resetTimer);
+			window.removeEventListener('keypress', resetTimer);
+			window.removeEventListener('mousemove', resetTimer);
+			window.removeEventListener('scroll', resetTimer);
+		});
+	}
+  onMount(() => { 
+    document.addEventListener('contextmenu', function (event) {
+      event.preventDefault();
     });
-
-    onDestroy(() => {
-      if (timeoutId !== undefined) {
-        clearTimeout(timeoutId);
-      }
-      window.removeEventListener('click', resetTimer);
-      window.removeEventListener('keypress', resetTimer);
-      window.removeEventListener('mousemove', resetTimer);
-      window.removeEventListener('scroll', resetTimer);
-    });
-  }
-
-  document.addEventListener('contextmenu', function(event) {
-    event.preventDefault();
-  });
-
-
-
-
-
-
-
-  
+    
+   })
 </script>
 
 <main class="min-h-[calc(100vh-3em)]">
